931f72b3d3c5efc69b883051f19a352242c6b61f
jonghyouk yun <ageldama@gmail.com>
2026-05-30 11:19:54 +0900

    --wip-- [skip ci]

--- src/main_aux.c
+++ src/main_aux.c
@@ -1,47 +1,7 @@
 #include "errmsg.h"
 #include "exec.h"
-#include "file_find.h"
 #include "main.h"
 #include <string.h>
-
-void
-compile_file_regex_or_not (void)
-{
-  if (NULL == argp.file_regex)
-    return;
-
-  p_regex = malloc (sizeof (regex_t));
-
-  char *errmsg = file_find_regex_compile (p_regex, argp.file_regex);
-
-  if (NULL != errmsg)
-    {
-      fputs (errmsg, stderr);
-      fputs ("\n", stderr);
-      ERRMSG_FREE (errmsg);
-      exit (EXIT_FAILURE);
-    }
-}
-
-void
-list_script_files (void)
-{
-  utarray_new (script_files, &ut_str_icd);
-
-  file_find_args_t file_find_args = {
-    .dir_matcher = NULL,
-    .dir_matcher_closure = NULL,
-    .file_matcher
-    = p_regex == NULL ? file_find_only_file : file_find_file_with_regex,
-    .file_matcher_closure = p_regex == NULL ? NULL : p_regex,
-    .recurse = true,
-  };
-
-  file_find_list_multiple (script_files, argp_get_script_dirs_owned (&argp),
-                           &file_find_args);
-
-  db_sort_by_last_epoch_desc (p_db, script_files);
-}
 
 void
 load_or_not (void)
